f78ccfad804eb5bf1ad36668611e54e9cdc1a388
wagner <wagncarv@outlook.com>
2024-02-26 00:23:04 -0300

    Store qr_code, get qr_code stored routes

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # REal Time Pay
 
 
+### Dependencies
+- pip3 install -r requirements.txt
+
+
 ### Create Database
 - flask shell
 - db.create_all()
--- payments/pix.py
+++ payments/pix.py
@@ -2,16 +2,23 @@ import uuid
 import qrcode
 
 class Pix:
-        def __init__(self):
-             pass
+       def __init__(self):
+              pass
         
-        def create_payment(self):
-               bank_payment_id = uuid.uuid4()
+       def create_payment(self):
+               # Criar o pagamento na instuição financeira
+               bank_payment_id = str(uuid.uuid4())
+
+               # Código copia e cola
                hash_payment = f'hash_payment_{bank_payment_id}'
+
+               # qr_code
                img = qrcode.make(hash_payment)
+
+               # Gravar imagem como arquivo .png
                img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
 
                return {
-                      "payment_bank_id": bank_payment_id,
+                      "bank_payment_id": bank_payment_id,
                       "qr_code_path": f"qr_code_payment_{bank_payment_id}"
                }
