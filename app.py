196f96b1d9930ff779f077cc939f423f3e5a0353
wagner <wagncarv@outlook.com>
2024-03-02 00:56:30 -0300

    Websocket confirmation

--- app.py
+++ app.py
@@ -42,18 +42,49 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
+    data = request.get_json()
+
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found"}), 404
+    
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment data"}), 400
+    
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
     return jsonify({"message": "The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
-    print(payment.__dict__)
+    
+    if not payment:
+        return render_template('404.html')
 
-    return render_template('payment.html', payment_id=payment.id, value=payment.value, host='http://localhost:5000', qr_code=payment.qr_code)
+    if payment.paid:
+        return render_template('confirmed_payment.html', 
+                            payment_id=payment.id, 
+                           value=payment.value, )
+
+    return render_template('payment.html', 
+                           payment_id=payment.id, 
+                           value=payment.value, 
+                           host='http://localhost:5000', 
+                           qr_code=payment.qr_code)
 
 @socketio.on('connect')
 def hanldle_connect():
     print("Client connected!")
+
+@socketio.on('disconnect')
+def hanldle_disconnect():
+    print("Client disconnected from server!")
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
